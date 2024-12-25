ae58875dcfa7bbcd893c4cea2238a157a0d78fc5
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-12-25 12:59:29 +0300

    Modified: Add an Image Composable.

--- app/src/main/java/com/umutyusufcinar/happybirthday/MainActivity.kt
+++ app/src/main/java/com/umutyusufcinar/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,11 +15,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.umutyusufcinar.happybirthday.ui.theme.HappyBirthdayTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,10 +64,19 @@ fun GreetingText(message: String, fromWho:String, modifier: Modifier = Modifier)
     }
 }
 
+@Composable
+fun GreetingImage(message: String, fromWho: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null,
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingText("Happy Birthday", fromWho = "UycDev")
+        GreetingImage("Happy Birthday", fromWho = "UycDev")
     }
 }
